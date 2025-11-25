cf646b8fe474f19756e7b624d5628b6e2329ab9a
Nourvic <nourvic7@gmail.com>
2025-11-25 22:21:24 +0100

    delete request

--- src/main/java/com/examplecodenour/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/examplecodenour/employeemanagement/controller/EmployeeController.java
@@ -12,9 +12,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    List<Employee> employees = new ArrayList(List.of(new Employee(UUID.randomUUID(), "Nour", "Salim", "noursalim@gmail.com", "017297xxxx", LocalDate.now(), "DevOps", UUID.randomUUID()), new Employee(UUID.randomUUID(), "Mohamad", "Salim", "mohamadsalim@gmail.com", "017297xxxx", LocalDate.now(), "Cyber", UUID.randomUUID())
-
-    ));
+    List<Employee> employees = new ArrayList(List.of(new Employee(UUID.randomUUID(), "Nour", "Salim", "noursalim@gmail.com", "017297xxxx", LocalDate.now(), "DevOps", UUID.randomUUID()), new Employee(UUID.randomUUID(), "Mohamad", "Salim", "mohamadsalim@gmail.com", "017297xxxx", LocalDate.now(), "Cyber", UUID.randomUUID())));
 
     @GetMapping()
     public List<Employee> HelloWold() {
@@ -34,5 +32,13 @@ public class EmployeeController {
         employees.add(employee);
         return employee;
     }
-    
+
+    @DeleteMapping("/{employeeId}")
+    public void deleteOne(@PathVariable UUID employeeId) {
+        Optional<Employee> emp = employees.stream().filter(e -> e.getId().equals(employeeId)).findFirst();
+
+        if (emp.isPresent()) {
+            employees.remove(emp.get());
+        }
+    }
 }
