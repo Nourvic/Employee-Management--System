24092c7ef00dfa161c603ee9f11acd5b4252d4c8
Nourvic <nourvic7@gmail.com>
2025-11-30 20:31:37 +0100

    adding exceptions customization and change some methodsbody

--- src/main/java/com/examplecodenour/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/examplecodenour/employeemanagement/controller/EmployeeController.java
@@ -1,12 +1,13 @@
 package com.examplecodenour.employeemanagement.controller;
 
 import com.examplecodenour.employeemanagement.entities.Employee;
+import com.examplecodenour.employeemanagement.shared.CustomResponseException;
+import com.examplecodenour.employeemanagement.shared.GlobalResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,26 +16,30 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    List<Employee> employees = new ArrayList(List.of(new Employee(UUID.randomUUID(), "Nour", "Salim", "noursalim@gmail.com", "017297xxxx", LocalDate.now(), "DevOps", UUID.randomUUID()), new Employee(UUID.randomUUID(), "Mohamad", "Salim", "mohamadsalim@gmail.com", "017297xxxx", LocalDate.now(), "Cyber", UUID.randomUUID())));
+    List<Employee> employees = new ArrayList<>();
 
     @GetMapping()
-    public ResponseEntity<List<Employee>> HelloWold() {
+    public ResponseEntity<GlobalResponse<List<Employee>>> findAll() {
         // ResponseEntity status vom Code kontrollieren falls beispielsweise ein Angestellter nicht vorhanden dann kann ich 404 schreiben
-        return new ResponseEntity<List<Employee>>(employees, HttpStatus.OK);
+        return new ResponseEntity<>(new GlobalResponse<>(employees), HttpStatus.OK);
     }
 
+
     @GetMapping("/{employeeId}")
-    public ResponseEntity<Employee> findOne(@PathVariable UUID employeeId) {
-        Optional<Employee> emp = employees.stream().filter(e -> e.getId().equals(employeeId)).findFirst();
-        return emp.isPresent() ? new ResponseEntity<>(emp.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<GlobalResponse<Employee>> findOne(@PathVariable UUID employeeId) {
+        Employee emp = employees.stream().filter(e -> e.getId().equals(employeeId)).findFirst().orElseThrow(() -> CustomResponseException.ResourceNotFound("Employee id with " + employeeId + " not found"));
+//  Optional 2      if (emp.isEmpty()) {
+//            throw CustomResponseException.ResourceNotFound("Employee id with " + employeeId + " not found");
+//        }
+        return new ResponseEntity<>(new GlobalResponse<>(emp), HttpStatus.OK);
     }
 
     @PostMapping()
-    public ResponseEntity<Employee> createOne(@Valid @RequestBody Employee employee) {
+    public ResponseEntity<GlobalResponse<Employee>> createOne(@Valid @RequestBody Employee employee) {
         employee.setId(UUID.randomUUID());
         employee.setDepartmentId(UUID.randomUUID());
         employees.add(employee);
-        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+        return new ResponseEntity<>(new GlobalResponse<>(employee), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{employeeId}")
@@ -46,17 +51,15 @@ public class EmployeeController {
 
     @PutMapping("/{employeeId}")
     public ResponseEntity<Employee> updateOne(@PathVariable UUID employeeId, @Valid @RequestBody Employee employee) {
-        Optional<Employee> excemp = employees.stream().filter(e -> e.getId().equals(employeeId)).findFirst();
-        return excemp.map(e -> {
-            e.setFirstName(employee.getFirstName());
-            e.setLastName(employee.getLastName());
-            e.setEmail(employee.getEmail());
-            e.setPhoneNumber(employee.getPhoneNumber());
-            e.setPosition(employee.getPosition());
-            e.setHireDate(employee.getHireDate());
-            e.setDepartmentId(employee.getDepartmentId());
-            return new ResponseEntity<>(e, HttpStatus.OK);
-        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        Employee excemp = employees.stream().filter(e -> e.getId().equals(employeeId)).findFirst().orElseThrow(() -> CustomResponseException.ResourceNotFound("Employee id with " + employeeId + " not found"));
 
+        excemp.setFirstName(employee.getFirstName());
+        excemp.setLastName(employee.getLastName());
+        excemp.setEmail(employee.getEmail());
+        excemp.setPhoneNumber(employee.getPhoneNumber());
+        excemp.setPosition(employee.getPosition());
+        excemp.setHireDate(employee.getHireDate());
+        excemp.setDepartmentId(employee.getDepartmentId());
+        return new ResponseEntity<>(excemp, HttpStatus.OK);
     }
 }
