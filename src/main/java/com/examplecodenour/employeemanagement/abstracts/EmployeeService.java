1592e130075b5c3b6f0881e22e9f5449f4e0ee8e
Nourvic <nourvic7@gmail.com>
2025-12-02 18:39:14 +0100

    creating Service class and Interface

---
+++ src/main/java/com/examplecodenour/employeemanagement/abstracts/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.examplecodenour.employeemanagement.abstracts;
+
+import com.examplecodenour.employeemanagement.entities.Employee;
+
+public interface EmployeeService {
+
+    Employee addOne(Employee employee);
+
+}
---
+++ src/main/java/com/examplecodenour/employeemanagement/services/EmployeeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.examplecodenour.employeemanagement.services;
+
+import com.examplecodenour.employeemanagement.abstracts.EmployeeService;
+import com.examplecodenour.employeemanagement.entities.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    Employee addOne(Employee employee) {
+        return employee;
+    }
+}
