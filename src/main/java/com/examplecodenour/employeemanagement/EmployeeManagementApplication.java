772955cd8424e4c942e501a3bdab5b42bb16a065
Nourvic <nourvic7@gmail.com>
2025-12-06 16:17:21 +0100

    configuration to DB via postgresql

--- src/main/java/com/examplecodenour/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/examplecodenour/employeemanagement/EmployeeManagementApplication.java
@@ -1,11 +1,7 @@
 package com.examplecodenour.employeemanagement;
 
-import com.examplecodenour.employeemanagement.entities.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.util.UUID;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
--- src/main/java/com/examplecodenour/employeemanagement/entities/Employee.java
+++ src/main/java/com/examplecodenour/employeemanagement/entities/Employee.java
@@ -1,24 +1,48 @@
 package com.examplecodenour.employeemanagement.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "employee")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Employee {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
     private UUID id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 150)
     private String lastName;
+
+    @Column(name = "email", nullable = false, length = 100, unique = true)
     private String email;
+
+    @Column(name = "phone_number", length = 25)
     private String phoneNumber;
+
+    @Column(name = "hire_date", nullable = false)
     private LocalDate hireDate;
+
+    @Column(name = "position", nullable = false)
     private String position;
+
+    @Column(name = "department_id", nullable = false)
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
     private UUID departmentId;
 }
