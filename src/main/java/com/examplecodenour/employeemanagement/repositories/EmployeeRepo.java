c0d349c65f5891a6531f22618e46c518ed0b6fca
Nourvic <nourvic7@gmail.com>
2025-12-06 17:38:55 +0100

    adding Repo to app

---
+++ src/main/java/com/examplecodenour/employeemanagement/repositories/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package com.examplecodenour.employeemanagement.repositories;
+
+import com.examplecodenour.employeemanagement.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, UUID> {
+
+
+}
--- src/main/java/com/examplecodenour/employeemanagement/services/EmployeeServiceImpl.java
+++ src/main/java/com/examplecodenour/employeemanagement/services/EmployeeServiceImpl.java
@@ -4,31 +4,30 @@ import com.examplecodenour.employeemanagement.abstracts.EmployeeService;
 import com.examplecodenour.employeemanagement.dto.EmployeeCreate;
 import com.examplecodenour.employeemanagement.dto.EmployeeUpdate;
 import com.examplecodenour.employeemanagement.entities.Employee;
+import com.examplecodenour.employeemanagement.repositories.EmployeeRepo;
 import com.examplecodenour.employeemanagement.shared.CustomResponseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     List<Employee> employees = new ArrayList<>();
 
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
     @Override
     public List<Employee> findAll() {
-        return employees;
+        return employeeRepo.findAll();
     }
 
     @Override
     public Employee findOne(UUID employeeId) {
-        return employees.stream()
-                .filter(e -> e.getId()
-                        .equals(employeeId))
-                .findFirst()
-                .orElseThrow(() -> CustomResponseException
-                        .ResourceNotFound("Employee id with " + employeeId + " not found"));
+        return employees.stream().filter(e -> e.getId().equals(employeeId)).findFirst().orElseThrow(() -> CustomResponseException.ResourceNotFound("Employee id with " + employeeId + " not found"));
 
         //   Employee emp = employees.stream() ................ Logik ->>> BlaBLaBla
 //      Or if (emp.isEmpty()) {
@@ -38,35 +37,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public void deleteOne(UUID employeeId) {
-        Optional<Employee> emp = employees
-                .stream().
-                filter(e -> e.getId().equals(employeeId))
-                .findFirst();
-        emp.ifPresent(e -> employees.remove(e));
+        Employee emp = employeeRepo.findById(employeeId)
+                .orElseThrow(() -> CustomResponseException.ResourceNotFound("Employee with" + employeeId + "not found"));
+        employeeRepo.delete(emp);
     }
 
     @Override
     public Employee createOne(EmployeeCreate employeeDto) {
         Employee empl = new Employee();
-        empl.setId(UUID.randomUUID());
-        empl.setDepartmentId(UUID.randomUUID());
         empl.setFirstName(employeeDto.firstName());
         empl.setLastName(employeeDto.lastName());
         empl.setEmail(employeeDto.email());
         empl.setPhoneNumber(employeeDto.phoneNumber());
         empl.setPosition(employeeDto.position());
         empl.setHireDate(employeeDto.hireDate());
-        employees.add(empl);
+        employeeRepo.save(empl);
         return empl;
     }
 
     @Override
     public Employee updateOne(UUID employeeId, EmployeeUpdate employee) {
-        Employee excemp = employees.stream()
-                .filter(e -> e.getId().equals(employeeId))
-                .findFirst()
-                .orElseThrow(() -> CustomResponseException
-                        .ResourceNotFound("Employee id with " + employeeId + " not found"));
+        Employee excemp = employees.stream().filter(e -> e.getId().equals(employeeId)).findFirst().orElseThrow(() -> CustomResponseException.ResourceNotFound("Employee id with " + employeeId + " not found"));
 
         excemp.setFirstName(employee.firstName());
         excemp.setLastName(employee.lastName());
