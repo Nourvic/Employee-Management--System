ca39e8e97ba7efc80bd64df8df2c945c01f9a3d2
Nourvic <nourvic7@gmail.com>
2026-01-18 18:32:55 +0100

    signup request record

--- src/main/java/com/examplecodenour/employeemanagement/dto/SignupRequest.java
+++ src/main/java/com/examplecodenour/employeemanagement/dto/SignupRequest.java
@@ -1,4 +1,18 @@
 package com.examplecodenour.employeemanagement.dto;
 
-public record SignupRequest() {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+        @NotNull(message = "username is required")
+        @Size(min = 2, max = 50, message = "Min 2 max 50 characters")
+        String username,
+
+        @NotNull(message = "password is required")
+        @Size(min = 8, max = 50, message = "between 8 and 50 ")
+        String password,
+
+        @NotNull(message = "employee id is required")
+        String employeeId
+) {
 }
