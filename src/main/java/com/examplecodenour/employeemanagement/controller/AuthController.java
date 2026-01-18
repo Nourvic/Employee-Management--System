fa6daca58972e7542fbf0f5aec992231e9f25573
Nourvic <nourvic7@gmail.com>
2026-01-18 18:35:36 +0100

    test the endpoint:

--- src/main/java/com/examplecodenour/employeemanagement/controller/AuthController.java
+++ src/main/java/com/examplecodenour/employeemanagement/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.examplecodenour.employeemanagement.controller;
 
+import com.examplecodenour.employeemanagement.dto.SignupRequest;
 import com.examplecodenour.employeemanagement.shared.GlobalResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @PostMapping("/signup")
-    public ResponseEntity<GlobalResponse<String>> signUp() {
+    public ResponseEntity<GlobalResponse<String>> signUp(@RequestBody SignupRequest signupRequest) {
+        
         return new ResponseEntity<>(new GlobalResponse<>("Signed Up"), HttpStatus.OK);
     }
 }
